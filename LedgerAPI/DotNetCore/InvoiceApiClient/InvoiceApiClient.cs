d95238b9edc88d84a08bde19a079489ced7c790c
Michael Maier <eksek@live.com>
2020-09-01 15:25:02 +0300

    Reorganized folders and refactored the token acquisition

---
+++ LedgerAPI/DotNetCore/InvoiceApiClient/InvoiceApiClient.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using RestSharp.Serialization.Json;
+
+namespace InvoiceApiClient
+{
+    public class InvoiceApiClient
+    {
+        private string AccessTokenUrl { get; }
+        private string ClientId { get; }
+        private string ClientSecret { get; }
+        private RestClient RestClient { get; }
+
+        public InvoiceApiClient(string accessTokenUrl, string clientId, string clientSecret, string host)
+        {
+            AccessTokenUrl = accessTokenUrl;
+            ClientId = clientId;
+            ClientSecret = clientSecret;
+            RestClient = new RestClient(new JsonDeserializer(), host);
+        }
+
+        public JsonArray GetInvoices(string resource)
+        {
+            var token = GetAccessToken().Result;
+            var request = new RestRequest(resource, Method.GET, null, token);
+            return RestClient.Call<JsonArray>(request);
+        }
+
+        private async Task<string> GetAccessToken()
+        {
+            var tokenClient = new HttpClient();
+            var clientCredentialsResponse = await tokenClient.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest()
+            {
+                Address = AccessTokenUrl,
+                ClientId = ClientId,
+                ClientSecret = ClientSecret,
+                Scope = "scope"
+            });
+            var token =  clientCredentialsResponse.AccessToken;
+            return token;
+        }
+    }
+}
---
+++ LedgerAPI/DotNetCore/InvoiceApiClientSample/InvoiceApiClientSample.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace InvoiceApiClientSample
+{
+    class InvoiceApiClientSample
+    {
+
+        public static readonly string apiHost = "https://api.lowell.com";
+        public static readonly string apiBasePath = "fin/qa/invoice/ledger";
+        public static readonly string tokenEndpoint = "https://idp.lowell.io/auth/realms/test-clients/protocol/openid-connect/token";
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("InvoiceApiClientSample needs 3 arguments: ");
+                Console.WriteLine("1. client id");
+                Console.WriteLine("2. client secret");
+                Console.WriteLine("3. customer id (to get invoices from)");
+            }
+            else
+            {
+                try
+                {
+                    var client = new InvoiceApiClient.InvoiceApiClient(tokenEndpoint, args[0], args[1], apiHost);
+                    var customerId = args[2]?.ToString();
+
+                    var resource = $"{apiBasePath}/v1/customers/{customerId}/invoices";
+
+                    Console.WriteLine(client.GetInvoices(resource));
+                }
+                catch (Exception e)
+                {
+                    Console.Write("Exception occurred: " + e.Message);
+                }
+                
+            }
+        }
+    }
+}
